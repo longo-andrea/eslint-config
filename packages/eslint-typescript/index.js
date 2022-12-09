c95464860cb5e6ddfc678d6e183fd7801e21a798
longo-andrea <longoandrea.dev@gmail.com>
2022-12-09 16:05:45 +0100

    feat: add eslint typescript configuration

---
+++ packages/eslint-typescript/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+
+	plugins: ['@typescript-eslint'],
+
+	extends: [
+		'@longo-andrea/eslint-base',
+		'plugin:@typescript-eslint/recommended',
+	],
+};
