3a1762de945604326255276f6966093dd9794869
longo-andrea <longoandrea.dev@gmail.com>
2022-12-09 19:40:27 +0100

    build: add publish scripts

---
+++ scripts/publish.mjs
@@ -0,0 +1,14 @@
+import fs from 'fs-extra';
+import execSync from 'child_process';
+
+const publishPackges = async () => {
+	const allPackages = await fs.readdir('./packages');
+	for (const pkg of allPackages) {
+		execSync('npm publish --access public', {
+			stdio: 'inherit',
+			cwd: path.join('packages', pkg),
+		});
+	}
+};
+
+await publishPackges();
