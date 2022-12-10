80c494809def23ccab62bb5fdef092d07e2f787d
longo-andrea <longoandrea.dev@gmail.com>
2022-12-10 11:21:18 +0100

    fix: publish script by using pnpm rather than npm

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -5,7 +5,7 @@ import { execSync } from 'child_process';
 const publishPackges = async () => {
 	const allPackages = await fs.readdir('./packages');
 	for (const pkg of allPackages) {
-		execSync('npm publish --access public', {
+		execSync('pnpm publish --access public', {
 			stdio: 'inherit',
 			cwd: path.join('packages', pkg),
 		});
