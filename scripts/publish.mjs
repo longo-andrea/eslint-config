54acfc587cda3e3bcd6db93df4a886056de56423
longo-andrea <longoandrea.dev@gmail.com>
2022-12-10 16:07:28 +0100

    ci: update publish ci GH action

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -1,15 +1,5 @@
-import fs from 'fs-extra';
-import path from 'path';
 import { execSync } from 'child_process';
 
-const publishPackges = async () => {
-	const allPackages = await fs.readdir('./packages');
-	for (const pkg of allPackages) {
-		execSync('pnpm publish --access public', {
-			stdio: 'inherit',
-			cwd: path.join('packages', pkg),
-		});
-	}
-};
-
-await publishPackges();
+execSync('pnpm -r publish --access public', {
+	stdio: 'inherit',
+});
